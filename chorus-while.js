6bb5e2a54893134d9400084921c75d65ab5c2de1
victor poirier <poiriervictor99@gmail.com>
2020-01-16 19:39:19 +0000

    added hello.js

---
+++ chorus-while.js
@@ -0,0 +1,13 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
